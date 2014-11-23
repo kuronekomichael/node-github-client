80c51d4a53a43f6a80839c455a5a4252c7a223f3
kuronekomichael <michael@cat.email.ne.jp>
2014-11-23 20:32:26 +0900

    change auth.user/pass to token

--- test/test-client.js
+++ test/test-client.js
@@ -1,6 +1,21 @@
 var Client = require('../lib/client');
 
-describe('api.github.com', function() {
+describe('pullrequests - api.github.com', function() {
+    var client;
+    before(function() {
+        client = new Client();
+        //client.authenticate('');
+    });
+    it('get', function(done) {
+        client.getPullRequests('kuronekomichael/node-doit-im', function(err, content) {
+            expect(err).is.not.ok;
+            expect(content).is.ok;
+            done();
+        });
+    });
+});
+
+describe('content - api.github.com', function() {
     var client;
     before(function() {
         client = new Client();
